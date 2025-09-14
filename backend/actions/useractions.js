ad0619752a5e917b7690eb4d2cc5802391673933
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-09-14 10:38:16 +0530

    Update useractions.js

--- backend/actions/useractions.js
+++ backend/actions/useractions.js
@@ -9,18 +9,6 @@ export const initiate = async (amount, to_username, paymentform) => {
   try {
     await connectDb();
 
-    var instance = new Razorpay({
-      key_id: process.env.NEXT_PUBLIC_KEY_ID,
-      key_secret: process.env.NEXT_PUBLIC_KEY_SECRET,
-    });
-
-    let options = {
-      amount: Number.parseInt(amount),
-      currency: "INR",
-    };
-
-    let x = await instance.orders.create(options);
-
     // create a payment object which shows a pending payment in the database
     await Payment.create({
       oid: x.id,
